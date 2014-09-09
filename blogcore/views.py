9c5b1145c528eb94a8426a2568c70aa3c03e43af
Yee <cittie.yi@gmail.com>
2014-09-09 20:53:39 +0800

    done

--- blogcore/views.py
+++ blogcore/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
 from blogcore.models import Post, Comment
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = "blogcore/index.html"
     context_object_name = "blog_list"
     
     def get_queryset(self):
-        return Post.objects.all().order_by("-created_date")[:10]
+        return Post.objects.order_by("-created_date")[:10]
 
 class DetailView(generic.DetailView):
+    model = Post
     template_name = "blogcore/detail.html"
     
     def get_queryset(self):
-        return Post.objects.filter()
+        return Post.objects.filter(created_date__lte = timezone.now())
