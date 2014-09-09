70072ed90e7547ec74755bb77dfcc1e6a4265154
Yee <cittie.yi@gmail.com>
2014-09-09 19:31:21 +0800

    Pages done

--- blogcore/views.py
+++ blogcore/views.py
@@ -3,11 +3,14 @@ from django.views import generic
 from blogcore.models import Post, Comment
 
 class IndexView(generic.ListView):
+    template_name = "blogcore/index.html"
+    context_object_name = "blog_list"
     
     def get_queryset(self):
-        return Post.objects.all()[:5]
+        return Post.objects.all().order_by("-created_date")[:10]
 
-class DetailView(generic.ListView):
+class DetailView(generic.DetailView):
+    template_name = "blogcore/detail.html"
     
     def get_queryset(self):
         return Post.objects.filter()
